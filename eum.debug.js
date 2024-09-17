fcac20c28f981d424de428f8192e7fb32ec7ee22
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-09-17 15:40:15 +0530

    test

--- eum.debug.js
+++ eum.debug.js
@@ -1738,13 +1738,17 @@
   // See https://www.w3.org/TR/hr-time/
 
   function addResourceTimings(beacon, minStartTime) {
-    console.log('beacon', beacon);
-    console.log('minStartTime', minStartTime);
+    console.log('beacon - ', beacon);
+    console.log('minStartTime - ', minStartTime);
+    console.log('isResourceTimingAvailable - ', isResourceTimingAvailable, !!isResourceTimingAvailable);
+    console.log('win.JSON - ', win.JSON);
+    console.log('performance.getEntriesByType - ', performance$1.getEntriesByType('resource'));
 
     if (!!isResourceTimingAvailable && win.JSON) {
       var _entries = getEntriesTransferFormat(performance$1.getEntriesByType('resource'), minStartTime);
 
       mapMetaData();
+      console.log('win.JSON.stringify(entries)', win.JSON.stringify(_entries));
       beacon['res'] = win.JSON.stringify(_entries);
     } else {
       info('Resource timing not supported.');
